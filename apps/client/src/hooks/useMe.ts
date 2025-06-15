3e079e236c9057290996d1fa1090219c98ed7d0c
Leechaeun1 <lce001105@gmail.com>
2025-06-15 19:58:37 +0900

    fix(mypage): change logic using a hooks

--- apps/client/src/hooks/useMe.ts
+++ apps/client/src/hooks/useMe.ts
@@ -1,4 +1,9 @@
-import { useQuery, useQueryClient, type QueryObserverResult, type UseQueryOptions } from '@tanstack/react-query';
+import {
+  useQuery,
+  useQueryClient,
+  type QueryObserverResult,
+  type UseQueryOptions,
+} from '@tanstack/react-query';
 
 import { useRouter } from 'next/navigation';
 import { useAuthStore } from 'src/lib/store/auth';
@@ -7,6 +12,7 @@ import { isClient } from 'src/utils/client';
 // import { getMeApi } from 'src/services/user';
 import { Notify } from '@ui/components';
 import { HELPER_MESSAGES } from '@libs/utils/message';
+import { WithdrawApi } from '@/services/withdraw';
 
 type QueryKey = ['getMe'];
 type Option = Partial<UseQueryOptions<User, Error, User, QueryKey>>;
@@ -16,6 +22,7 @@ interface UseMe {
   error: unknown;
   refetchMe: () => Promise<QueryObserverResult<User, Error>>;
   onLogout(): void;
+  onWithdraw(): Promise<void>;
   isLoading: boolean;
 }
 
@@ -50,7 +57,10 @@ const useMe = (options?: Option): UseMe => {
       } catch (err) {
         setUserData(null);
 
-        const message = err instanceof Error ? err.message : 'An error occurred while fetching user data';
+        const message =
+          err instanceof Error
+            ? err.message
+            : 'An error occurred while fetching user data';
 
         Notify.error(message);
         throw err;
@@ -69,11 +79,31 @@ const useMe = (options?: Option): UseMe => {
     Notify.success(HELPER_MESSAGES.logoutSuccess);
   };
 
+  const onWithdraw = async () => {
+    if (!window.confirm('정말 탈퇴하시겠습니까?')) {
+      return;
+    }
+    try {
+      await WithdrawApi();
+      console.log('탈퇴 성공');
+      Notify.success('이용해주셔서 감사합니다.');
+      setUserData(null);
+      queryClient.clear();
+      router.replace('/');
+    } catch (error: any) {
+      console.error('회원 탈퇴 오류 발생:', error);
+      const errorMessage = error.message || '알 수 없는 오류가 발생했습니다.';
+      Notify.error(`탈퇴 실패: ${errorMessage}`);
+      throw error;
+    }
+  };
+
   return {
     user,
     error,
     refetchMe,
     onLogout,
+    onWithdraw,
     isLoading,
   };
 };
