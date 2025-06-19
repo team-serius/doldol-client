a6ef50ce2221a615b26a52f322f8e955926c697b
Leechaeun1 <lce001105@gmail.com>
2025-06-19 13:06:33 +0900

    fix(landing): change image tag

--- apps/client/src/containers/landing/IntroSection.tsx
+++ apps/client/src/containers/landing/IntroSection.tsx
@@ -55,16 +55,6 @@ export const IntroSection = () => {
           </Typography>
         </div>
       </div>
-      <Link href={user ? "/my-page" : "/auth/login"} className="block">
-        <Button
-          variant={"secondary"}
-          size={"large"}
-          shape="circle"
-          className="mt-10"
-        >
-          지금 시작해보기
-        </Button>
-      </Link>
     </section>
   );
 };
